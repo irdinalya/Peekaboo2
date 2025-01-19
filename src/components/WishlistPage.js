554220e6b4b9c4a495982afea0661fc1defae96c
irdinalya <irdinalya3@gmail.com>
2025-01-20 00:01:06 +0800

    Update WishlistPage.js

--- src/components/WishlistPage.js
+++ src/components/WishlistPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import products from './products';
 import './WishlistPage.css';
 
 function WishlistPage({ wishlist, onRemove }) {
+    const wishlistImages = wishlist.map((item) => {
+        const product = products.find((p) => p.id === item.id);
+        return { ...item, image: product ? product.image : "/images/default-product.jpg" };
+    });
     return (
         <div className="wishlist-page">
             <h2>Your Wishlist</h2>
@@ -9,17 +14,13 @@ function WishlistPage({ wishlist, onRemove }) {
                 <p className="empty-wishlist">Your wishlist is empty.</p>
             ) : (
                 <ul className="wishlist-list">
-                    {wishlist.map((item) => (
+                    {wishlistImages.map((item) => (
                         <li key={item.id} className="wishlist-item">
-                            <img
-                                src={`/images/${item.image}`}
-                                alt={item.name}
-                                className="wishlist-image"
-                            />
+                            <img src={item.image} alt={item.name} className="product-image"/>
                             <div className="wishlist-details">
                                 <h3>{item.name}</h3>
                                 <p>{item.description}</p>
-                                <p className="wishlist-price">Price: ${item.price.toFixed(2)}</p>
+                                <p className="wishlist-price">Price: RM{item.price.toFixed(2)}</p>
                                 <button onClick={() => onRemove(item.id)} className="remove-button">
                                     Remove
                                 </button>
